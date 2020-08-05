52cc7c74f900939f332d91fab0298afe86e6bf32
Christie Yoon <cleex1999@gmail.com>
2020-08-05 14:15:35 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,9 +14,6 @@ def retrieval
                   "oil paintings" => 3
                  }
  shipping_manifest["oil paintings"]
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-
 end
 
 def adding
@@ -25,14 +22,6 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
-
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-
-
-
-  # return the shipping_manifest hash below
-
+ shipping_manifest = ["muskets"=>2, "gun powder"=>4]
+ shipping_manifest
 end
